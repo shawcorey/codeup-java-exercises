6c99cba462bb1b49bb08934ccd4cd79c64786a5c
shawcorey <shaw.corey@gmail.com>
2021-06-21 15:17:35 -0500

    initial push

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,7 @@
 public class HelloWorld {
+
+    public static void main(String[] args){
+        //ALL JAVA STRINGS MUST BE IN DOUBLE QUOTES!!!!!
+        System.out.println("Hello, world");
+    }
 }
