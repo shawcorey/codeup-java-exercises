dd427233879543b1d058256ebee3f9b26f9df81f
shawcorey <shaw.corey@gmail.com>
2021-06-21 17:41:10 -0500

    second push

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,23 @@ public class HelloWorld {
 
     public static void main(String[] args){
         //ALL JAVA STRINGS MUST BE IN DOUBLE QUOTES!!!!!
-        System.out.println("Hello, world");
+//        System.out.println("Hello, world");
+        byte myFirstNumber = 3.14;
+        //Error is recieved. Turns number into a double
+        byte myNumber = 123;
+        //Error code is shown as long
+        String myString = "Whats Up Doe";
+        String myString = 3.14159;
+
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+
+
     }
 }
---
+++ src/SyntaxTypesVariables.java
@@ -0,0 +1,14 @@
+public class SyntaxTypesVariables {
+
+    public static void main(String [] args) {
+         //CODE your object type
+        byte myFirstNumber = 127;
+        byte mySecondNumber = 1;
+        myFirstNumber += mySecondNumber;
+        System.out.println(myFirstNumber);
+//        System.out.println(((Object) (myFirstNumber + 1)).getClass().getName());
+      // short
+      // int
+    }
+
+}
