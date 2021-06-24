ff3e07f8805ff96240f80db6a9254e6c47407a62
shawcorey <shaw.corey@gmail.com>
2021-06-24 18:00:16 -0500

    created string exercise and bob file

---
+++ src/StringExercise.java
@@ -0,0 +1,23 @@
+public class StringExercise {
+
+    public static void main(String[] args) {
+
+        String noEducation = "We don't need no education\n" +
+                "We don't need no thought control";
+
+        System.out.println(noEducation);
+
+        String checkOut = "Check \"this\" out!, \"s inside of \"s!";
+
+        System.out.println(checkOut);
+
+        String mainDrive = "In windows, the main drive is usually C:\\";
+
+        System.out.println(mainDrive);
+
+        String multipleSlashes = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+
+        System.out.println(multipleSlashes);
+    }
+}
