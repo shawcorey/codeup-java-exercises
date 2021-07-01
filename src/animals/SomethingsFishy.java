0464540e8b6bf283891643d8487dd668bab9c8d3
shawcorey <shaw.corey@gmail.com>
2021-07-01 17:30:10 -0500

    created multiple class files

--- src/animals/SomethingsFishy.java
+++ src/animals/SomethingsFishy.java
@@ -4,8 +4,8 @@ public class SomethingsFishy {
 
     public static void main(String[] args) {
 
-        MarineAnimal marineAnimal = new AquaMammal("Blue Whale");
-
-        System.out.println(marineAnimal.set);
+//        MarineAnimal marineAnimal = new AquaMammal("Blue Whale");
+//
+//        System.out.println(marineAnimal.set);
     }
 }
