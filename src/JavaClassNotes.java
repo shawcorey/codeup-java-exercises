bfb9db5f692c54c9ae01a4f900a20af956d78835
shawcorey <shaw.corey@gmail.com>
2021-07-06 09:42:12 -0500

    updated class exercise

--- src/JavaClassNotes.java
+++ src/JavaClassNotes.java
@@ -74,3 +74,7 @@
 //public static String
 
 //
+//Create a new “warmup” package that includes three classes. The first class will be a Parent class with a method that prints “I’m the parent”. The second class will be its subclass with another method that prints “I’m the Subclass”. The third Solution class will hold the solution, where you, now, create an object for each of the classes and call the following methods :
+//        1 - method of parent class by object of parent class
+//2 - method of child class by object of child class
+//3 - method of parent class by object of child class
