a5bcc7f341bf8e2e767af7fc7f8fdef45b81325b
shawcorey <shaw.corey@gmail.com>
2021-07-08 16:08:25 -0500

    created files for array exercise

---
+++ src/warmpup/arraywarmup.java
@@ -0,0 +1,13 @@
+package warmpup;
+
+public class arraywarmup {
+
+    public static void main(String[] args) {
+        Object getIntArray;
+        System.out.println(arraywarmup.toString(getIntArray()));
+    }
+
+    private static boolean toString(Object getIntArray) {
+        return false;
+    }
+}
