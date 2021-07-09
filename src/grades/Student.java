e490906d5b9fa5677744356f9d49dce13dcbb29e
shawcorey <shaw.corey@gmail.com>
2021-07-09 14:18:00 -0500

    created grades exercise

---
+++ src/grades/Student.java
@@ -0,0 +1,12 @@
+package grades;
+
+public class Student {
+
+    // returns the student's name
+    public String getName();
+    // adds the given grade to the grades property
+    public void addGrade(int grade);
+    // returns the average of the students grades
+    public double getGradeAverage();
+
+}
