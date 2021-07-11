45d40971a1207494ac6d574792dd214037d7aa05
shawcorey <shaw.corey@gmail.com>
2021-07-11 16:29:44 -0500

    updated student and grades application app

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,5 +1,42 @@
 package grades;
 
+import java.util.HashMap;
+
 public class GradesApplication {
 
+    public static void main(String[] args) {
+        HashMap<String, Student> usernames = new HashMap<>();
+        //Creates students
+        Student nameOne = new Student("Micheal Jordan");
+        Student nameTwo = new Student("Larry Bird");
+        Student nameThree = new Student("Oscar Robertson");
+        Student nameFour = new Student("Isiah Thomas");
+        //Creates Students Grades
+        nameOne.addGrade(90);
+        nameTwo.addGrade(80);
+        nameThree.addGrade(70);
+        nameFour.addGrade(60);
+
+        nameOne.addGrade(70);
+        nameTwo.addGrade(60);
+        nameThree.addGrade(90);
+        nameFour.addGrade(80);
+
+        nameOne.addGrade(80);
+        nameTwo.addGrade(90);
+        nameThree.addGrade(70);
+        nameFour.addGrade(60);
+        //Sets username with .put ---set a key value pair
+        usernames.put("Coding Bull", nameOne);
+        usernames.put("Coding Celtic", nameTwo);
+        usernames.put("Coding Triple Double", nameThree);
+        usernames.put("Coding Piston", nameFour);
+        System.out.println("Welcome!");
+        System.out.println("Here are the Github usernames of our Students:");
+
+        for(String key: usernames.keySet()){
+            System.out.print(" |" + key + "| ");
+        }
+
+    }
 }
--- src/grades/Student.java
+++ src/grades/Student.java
@@ -6,11 +6,28 @@ public class Student {
     private String name;
     private ArrayList<Integer> grade;
 
-    public Student(String name, ArrayList<Integer> grade) {
+    public Student(String name) {
         this.name = name;
-        this.grade = grade;
-    }
+        this.grade = new ArrayList<>();
 
+    }
+    // returns the student's name
+    public String getName(){
+        return name;
+    };
+    // adds the given grade to the grades property
+    public void addGrade(int grade){
+        this.grade.add(grade);
+    };
+    // returns the average of the students grades
+    public double getGradeAverage(){
+        int sum = 0;
+        for (int i = 0;  i < this.grade.size(); i++ ){
+            sum = sum + this.grade.get(i);
+        }
+        return(double) sum / this.grade.size();
+
+    };
     public Student() {
     }
 
