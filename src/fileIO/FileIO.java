7d3f2b2f61d4617d255c2fcd4ccb5d23f98e59e8
shawcorey <shaw.corey@gmail.com>
2021-07-15 14:59:53 -0500

    updated fileIO exercise

--- src/fileIO/FileIO.java
+++ src/fileIO/FileIO.java
@@ -10,37 +10,55 @@ public class FileIO {
     public static void main(String[] args) {
 //        System.out.println(getFilePath("fileIO","test.txt").toAbsolutePath());
         System.out.println("Does the path exist?");
-        System.out.println(getFilePath("src","fileIO","test.txt"));
+        System.out.println(getFilePath("src", "fileIO", "test.txt"));
 
 
     }
 
-    public static Path getFilePath(String filename){
+    public static Path getFilePath(String filename) {
         return Paths.get(filename);
     }
 
-    public static Path getFilePath(String parentDirectory, String filename){
+    public static Path getFilePath(String parentDirectory, String filename) {
         return Paths.get(parentDirectory, filename);
     }
 
-    public static Path getFilePath(String parentDirectory, String childDirectory, String filename){
+    public static Path getFilePath(String parentDirectory, String childDirectory, String filename) {
         return Paths.get(parentDirectory, childDirectory, filename);
     }
 
-    public static boolean doesPathExist(Path path){
+    public static boolean doesPathExist(Path path) {
         return Files.exists(path);
     }
 
-    public static void tryCreateDirectory(Path pathToCreate){
+    //    public static void tryCreateDirectory(Path pathToCreate){
+//
+//        if (!doesPathExist(pathToCreate)){
+//
+//            try {
+//                Files.createDirectory(pathToCreate);
+//            }catch (IOException e){}
+//            System.out.println("Could not create the directory at: ");
+//            System.out.println(pathToCreate.toAbsolutePath());
+//
+//        }
+//
+//    } else {
+//        System.out.println("The path at: " + pathToCreate.toAbsolutePath() + " already exists");
+//    }
+    public static void tryCreateDirectory(Path pathToCreate) {
 
-        if (!doesPathExist(pathToCreate)){
+        if (!doesPathExist(pathToCreate)) {
+
             try {
                 Files.createDirectory(pathToCreate);
-            }catch (IOException e){}
-            System.out.println("Could not create the directory at: ");
-            System.out.println(pathToCreate.toAbsolutePath());
+            } catch (IOException e) {
+                System.out.println("Could not create the directory at: ");
+                System.out.println(pathToCreate.toAbsolutePath());
+            }
 
+        } else {
+            System.out.println("The path at: " + pathToCreate.toAbsolutePath() + " already exists");
         }
-
     }
 }
