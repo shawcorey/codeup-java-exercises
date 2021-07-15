cddc13893a296b026b985ab7ef7f1c5eb4e1bcda
shawcorey <shaw.corey@gmail.com>
2021-07-15 10:18:39 -0500

    created and updated exception exercise

---
+++ src/Exception/WrongIndentationTypeException.java
@@ -0,0 +1,9 @@
+package Exception;
+// this is the custom Exception Class // this is the custom exception
+
+public class WrongIndentationTypeException extends Exception{
+
+    public WrongIndentationTypeException(String msg){
+
+    }
+}
---
+++ src/warmpup/July152021.java
@@ -0,0 +1,20 @@
+package warmpup;
+
+//WARM UP
+// TODO Given this string: `String str = "Oberon is the
+//  best cohort ever!"`,  that generates
+//  a number between 0 and 50 and print out the character from that
+//  string using a random number generator. Using a try/catch block, handle
+//  any errors (Exceptions) that may occur by printing out the
+//  message.
+
+public class July152021 {
+  public static void randomLetter(String ){
+
+      double randomLetter = Math.random();
+
+      System.out.println("Oberon is the best cohort ever!");
+  }
+}
+
+
