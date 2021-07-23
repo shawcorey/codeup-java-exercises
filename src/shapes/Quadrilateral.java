17eb1539262d626ebe27f49738daca57d4169da2
shawcorey <shaw.corey@gmail.com>
2021-07-22 21:32:08 -0500

    finalized quads exercise

--- src/shapes/Quadrilateral.java
+++ src/shapes/Quadrilateral.java
@@ -1,4 +1,33 @@
 package shapes;
 
-public class Quadrilateral {
+abstract class Quadrilateral extends Shape {
+    protected double length;
+    protected double width;
+
+    public Quadrilateral(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public abstract void setLength(double side);
+
+    public abstract void setWidth(double side);
+
+//    @Override
+//    public double getArea() {
+//        return 2 * (length + width);
+//    }
+//
+//    @Override
+//    public double getPerimeter() {
+//        return length * width;
+//    }
 }
